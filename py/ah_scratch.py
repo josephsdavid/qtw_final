af97cbdb92ea6df52efb17b7fbadd582606d7380
Landcruiser87 <andyheroy@gmail.com>
2020-04-14 01:48:28 -0500

    Testing hyperlinking to code appendix

--- py/ah_scratch.py
+++ py/ah_scratch.py
@@ -368,9 +368,4 @@ print("Confusion Matrix:\n", lr_confusion)
 print("Custom Cross Validation Score:\n", lr_1_score)
 print("Classification Report", classification_report(y_test, y_pred))
 
-#fuck
-# ehh work
-
-#fuckyou
-#fuckyouharder
-
+#fu
