a967c9ea392fc81437cf663b230072379f663431
Landcruiser87 <andyheroy@gmail.com>
2020-04-14 03:02:38 -0500

    fixing results table

--- py/ah_scratch.py
+++ py/ah_scratch.py
@@ -368,4 +368,6 @@ print("Confusion Matrix:\n", lr_confusion)
 print("Custom Cross Validation Score:\n", lr_1_score)
 print("Classification Report", classification_report(y_test, y_pred))
 
+#weeeeeeeeeee
+#we weeeeee
 
